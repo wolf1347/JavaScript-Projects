89be4213a0960d9102e2f127114376f5e578571d
Amber W <amber.wolf@outlook.com>
2020-02-10 12:25:04 -0700

    initial commit

---
+++ Basic_Javascript_Projects/Basic_JavaScript_1.js
@@ -0,0 +1,24 @@
+var y="This is a pop-up"
+window.alert(y);
+
+document.write("\"if done right these quotes should display\"");
+
+function myFunction() {
+var string1="This is a string ";
+var string2="and if done correctly ";
+var string3="these should all connect.";
+var res= string1.concat(string2, string3); 
+document.getElementById("demo").innerHTML=res;
+}
+
+var b= "This was a little easier" + " thank the other method I used " + "below with the button.";
+document.write(b);
+
+var pets= "The Wolf Pack", Cat="Jazz", Dog="Deku";
+document.write(Dog);
+
+var pupper="all dogs are good boys";
+var pupper=pupper.fontcolor("orange");
+document.write(pupper);
+
+Document.write(3 + 3);
